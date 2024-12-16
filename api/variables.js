7477aa067e252b3fa290b84136591746afaaf9ee
shaik-moazzam <shaikmoazzam1@gmail.com>
2024-12-17 02:46:20 +0530

    var

--- api/variables.js
+++ api/variables.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://brandingz.site/api/v1/";
-export const BACKEND = "http://brandingz.site:5000/";
+export const BACKEND = "https://brandingz.site/";
 export const BACK_KEY = "hdDnIKsddcsS3fsdn3DAdsf";
